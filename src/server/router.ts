4d9b972efa0997a339ae3fe1b6e0e3785fa008da
jcake <jakieschneider13@gmail.com>
2021-02-25 23:51:02 +1100

    Implement cleanliness API

--- src/server/router.ts
+++ src/server/router.ts
@@ -8,15 +8,22 @@ const router = Router();
 
 router.get('/get-day', async function (req, res) {
     try {
-        const response = ((day: string) => ({
-            friendly: `${req.query?.date ? "That would be" : "Today is"} ${day}`,
-            raw: day,
-            calculated: req.query && !!req.query?.date || false,
-            relative: req.query && (req.query?.relative === "true") || false,
-            offset: req.query?.date || null
-        }))(await getDay(Number(req.query?.date), req.query?.relative === "true"));
+        if ('presentation' in req.query) {
+            if (req.query.presentation === 'friendly')
+                res.end(`${req.query?.date ? "That would be" : "Today is"} ${await getDay(Number(req.query?.date), req.query?.relative === "true")}`);
+            else if (req.query.presentation === 'raw')
+                res.end(await getDay(Number(req.query?.date), req.query?.relative === "true"));
+        } else {
+            const response = ((day: string) => ({
+                friendly: `${req.query?.date ? "That would be" : "Today is"} ${day}`,
+                raw: day,
+                calculated: req.query && !!req.query?.date || false,
+                relative: req.query && (req.query?.relative === "true") || false,
+                offset: req.query?.date || null
+            }))(await getDay(Number(req.query?.date), req.query?.relative === "true"));
 
-        res.json(response);
+            res.json(response);
+        }
     } catch (err) {
         res.status(err.code);
         res.json({ok: false, message: err.message});
