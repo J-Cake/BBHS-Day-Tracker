65902023493604aff939396f3c933f8b661ec087
jcake <jakieschneider13@gmail.com>
2021-02-25 23:11:32 +1100

    Whoops

---
+++ src/API/getCurrentTime.ts
@@ -0,0 +1,25 @@
+import * as needle from 'needle';
+
+interface WorldTimeAPIResponse {
+    week_number: number,
+    utc_offset: string,
+    utc_datetime: string,
+    unixtime: number,
+    timezone: string,
+    raw_offset: number,
+    dst_until: string,
+    dst_offset: number,
+    dst_from: string,
+    dst: boolean,
+    day_of_year: number,
+    day_of_week: number,
+    datetime: string,
+    client_ip: string,
+    abbreviation: string
+}
+
+export default async function getCurrentTime(): Promise<number> {
+    const res: WorldTimeAPIResponse = (await needle('get', 'worldtimeapi.org/api/timezone/Australia/Sydney', {json: true})).body;
+
+    return res.unixtime * 1000; // doens't include milliseconds
+}
---
+++ src/server/day_tracker.ts
@@ -0,0 +1,33 @@
+import * as express from 'express';
+import * as path from "path";
+import * as fs from "fs";
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+
+import router from './router';
+import user from './user';
+import start from "./start";
+
+const app = express();
+
+const log = fs.createWriteStream(path.join(process.cwd(), ".request.log"), {flags: 'a', encoding: "utf8"});
+
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan("combined", {stream: log}));
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.use("/", user);
+app.use("/api", router);
+
+const port = 5493;
+
+app.listen(port, function (): void {
+    console.log("Listening on", port);
+    start();
+});
